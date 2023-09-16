e25b1ff46c770e275f3fdc33e235e0d13db46184
Tatyana <KrukovaTatyana1990@gmail.com>
2023-09-16 19:24:18 +0300

    Delete entry

--- Homework_8.py
+++ Homework_8.py
@@ -14,19 +14,24 @@
 # 4. Использование функций. Ваша программа
 # не должна быть линейной (наличие if и else)
 
+# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных.
+# Пользователь также может ввести имя или фамилию,
+# и Вы должны реализовать функционал для изменения и удаления данных и поиска по фамилии.
+
 from os.path import exists
 from csv import DictReader, DictWriter
+import os
 
 def get_info():
     info = []
-    first_name = 'Ivan'
-    last_name = 'Ivanov'
+    first_name = input('Введите имя: ')
+    last_name = input('Введите фамилию: ')
     info.append(first_name)
     info.append(last_name)
     flag = False
     while not flag:
         try:
-            phone_number = 12345678919 #int(input('Введите номер телефона: '))
+            phone_number = int(input('Введите номер телефона: '))
             if len(str(phone_number)) != 11:
                 print("wrong number!")
             else:
@@ -39,18 +44,18 @@ def get_info():
 def create_file():
     with open("phone.csv", "w", encoding='utf-8') as data:
         # data.write('Фамилия;Имя;Номер телефона\n')
-        f_n_writer = DictWriter(data, fieldnames =['Фамилия', 'Имя', 'Номер'])
+        f_n_writer = DictWriter(data, fieldnames =[ 'Имя', 'Фамилия', 'Номер'])
         f_n_writer.writeheader()
 
 def write_file(lst):
     # with open("phone.txt", "a", encoding='utf-8') as data:
     #     data.write(f'{lst[0]};{lst[1]};{lst[2]}\n')
     with open('phone.csv', 'r+', encoding='utf-8') as f_n:
-        fnreader = DictReader(f_n)
-        res = list(fnreader)
-        obj = {'Фамилия': lst[0], 'Имя': lst[1], 'Номер': lst[2]}
+        f_n_reader = DictReader(f_n)
+        res = list(f_n_reader)
+        obj = {'Имя': lst[0], 'Фамилия': lst[1], 'Номер': lst[2]}
         res.append(obj)
-        f_n_writer = DictWriter(f_n, fieldnames=['Фамилия', 'Имя', 'Номер'])
+        f_n_writer = DictWriter(f_n, fieldnames=['Имя', 'Фамилия', 'Номер'])
         for el in res:
             f_n_writer.writerow(el)
 
@@ -59,13 +64,28 @@ def read_file():
     #     phone_book = data.readlines()
     # return phone_book
     with open('phone.csv', encoding='utf-8') as f_n:
-        fnreader = DictReader(f_n)
-        phone_book = list(fnreader)
-    return phone_book
+        f_n_reader = DictReader(f_n)
+        phone_book = list(f_n_reader)
+        print(*phone_book[0].keys())
+        for el in phone_book:
+            print(*el.values())
 
 def record_info():
     lst = get_info()
     write_file(lst)
+
+def delete_entry(word):
+    phone_book = []
+    with open('phone.csv', encoding='utf-8') as f_n:
+        f_n_reader = DictReader(f_n)
+        phone_book = list(f_n_reader)
+    create_file()
+    with open('phone.csv', 'a', encoding='utf-8') as f_n:
+        for el in phone_book:
+            if el['Имя'] != word and el['Фамилия'] != word:
+                f_n_writer = DictWriter(f_n, fieldnames=['Имя', 'Фамилия', 'Номер'])
+                f_n_writer.writerow(el)
+
 
 def main(): #выполняет функцию файлового ???
     while 1: #или while True
@@ -76,12 +96,19 @@ def main(): #выполняет функцию файлового ???
             if not exists('phone.csv'):
                 print('Файл не создан!')
                 break
-            print(*read_file())
+            read_file()
         elif command == 'w':
             if not exists('phone.csv'):
                 create_file()
                 record_info()
             else:
                 record_info()
+        elif command == 'd':
+            if not exists('phone.csv'):
+                print('Файл не создан!')
+                break
+            else:
+                word = input("Введите имя или фамилию для удаления записи: ")
+                delete_entry(word)
 
 main()
