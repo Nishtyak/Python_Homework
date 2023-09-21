fac1f39909f34ff0f5e98b721320c92e119769c0
Tatyana <KrukovaTatyana1990@gmail.com>
2023-09-21 16:14:54 +0300

    Task 2(42)

--- Homework_9.py
+++ Homework_9.py
@@ -8,3 +8,10 @@ import pandas as pd
 data_file = pd.read_csv('sample_data/california_housing_train.csv')
 print(f'Cредняя стоимость дома, где кол-во людей от 0 до 500: ')
 print(data_file[(data_file['population'] >= 0) & (data_file['population'] <= 500)]['median_house_value'].mean())
+
+# Задача 42: Узнать какая максимальная households
+# в зоне минимального значения population.
+
+print()
+print('Максимальная households в зоне минимального значения population: ')
+print(data_file[data_file['population'] == data_file['population'].min()]['households'].max())
